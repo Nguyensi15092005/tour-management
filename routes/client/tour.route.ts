01e0e5b24a2cfb3e09ec49a2a699b5441aae7974
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-07-13 22:16:09 +0700

    Làm trang Danh sách tour theo danh mục

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -3,6 +3,10 @@ import * as controller from "../../controller/client/tour.controller";
 
 const router: Router = Router();
 
-router.get("/", controller.index);
+router.get("/:slugCategory", controller.index);
+
+router.get("/detail/:slugDetail", controller.detail);
+
+
 
 export const TourRoutes: Router = router;
