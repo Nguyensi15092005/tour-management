96d90a6df0fefc819df81fc78f62d3e67d02f9bc
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-07-13 16:14:09 +0700

    làm trang danh mục tour bên client

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,10 @@
 import { Express } from "express"
 import { TourRoutes } from "./tour.route";
+import { CategoryRoutes } from "./category.route";
+
+
 const clientRotes = (app: Express): void => {
     app.use("/tours", TourRoutes);
+    app.use("/categories", CategoryRoutes);
 };
 export default clientRotes;
