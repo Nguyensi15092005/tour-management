ac73feca1196c43d654579a88a7b1185a874c4f1
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-07-13 15:17:43 +0700

    cấu trúc folder PUG

--- controller/client/tour.controller.ts
+++ controller/client/tour.controller.ts
@@ -3,11 +3,17 @@ import Tour from "../../models/tour.model";
 
 
 export const index = async (req: Request, res: Response) => {
+  //SELECT * FROM tour WHERE deleted = false AND status = "active";
   const tours = await Tour.findAll({
+    where:{
+      deleted: false,
+      status: "active"
+    },
     raw: true // ko có các key lung tung
   });
   console.log(tours)
   res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
     tours: tours
   })
 } 
