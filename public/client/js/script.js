ec603d7f818a188c3b079a092105348744bf2db6
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-07-14 15:58:42 +0700

    thêm tour vào localstorage

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -18,3 +18,39 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 // End Slider swiperjs tour detail
+
+// Carts
+const cart = localStorage.getItem("cart");
+console.log(cart)
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+// thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const quantity = parseInt(e.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+    console.log(quantity, tourId);
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      
+      const isExitTour = cart.findIndex(item => item.tourId == tourId);
+      if (isExitTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        })
+      }
+      else {
+        cart[isExitTour].quantity = cart[isExitTour].quantity + quantity
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart))
+      console.log(cart)
+    }
+  })
+}
+// End Carts
+
